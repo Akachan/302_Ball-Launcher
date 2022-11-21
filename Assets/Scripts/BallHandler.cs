6f2dc2616d3d83567ffc31bdbd8e6ef11173ff56
Akachan <chaves.mayra@gmail.com>
2022-11-21 14:39:37 -0300

    Launching The Ball

--- Assets/Scripts/BallHandler.cs
+++ Assets/Scripts/BallHandler.cs
@@ -5,13 +5,19 @@ using UnityEngine.InputSystem;
 
 public class BallHandler : MonoBehaviour
 {
+   
     [SerializeField] Rigidbody2D currentBallRigidbody;
+    [SerializeField] SpringJoint2D currentBallSpringJoint;
+    [SerializeField] float detachBallDelay = 0.1f;
 
     Camera mainCamera;
+
+     bool isDragging;
 
     void Start() 
     {
         mainCamera = Camera.main;
+        
         
     }
     void Update()
@@ -19,9 +25,15 @@ public class BallHandler : MonoBehaviour
         //Mira si se esta tocando o no la pantalla. Si no se toca, mejor no hacer nada.
         if (!Touchscreen.current.primaryTouch.press.isPressed) 
         { 
-            currentBallRigidbody.isKinematic = false;
+            if(isDragging)
+            {
+                LaunchBall();
+            }
+            isDragging = false;
             return;
         }
+
+        isDragging = true;
         currentBallRigidbody.isKinematic = true;
         //Leer la pos del touch y guardarla
         Vector2 touchPosition = Touchscreen.current.primaryTouch.position.ReadValue();  
@@ -30,5 +42,22 @@ public class BallHandler : MonoBehaviour
         Vector3 worldPosition = mainCamera.ScreenToWorldPoint(touchPosition);
 
         currentBallRigidbody.position = worldPosition;
+    }
+    void LaunchBall()
+    {
+        //Lo vuelvo dinamico y elimino el objeto de la variable, para luego verificar
+        //que no lo vuelva a hacer si volvemos a touchear y soltar cuando la pelota ya se fue.
+        currentBallRigidbody.isKinematic = false;
+        currentBallRigidbody = null;
+
+        Invoke("DetachBall", detachBallDelay);
+
+    }
+
+    private void DetachBall()
+    {
+        //rompo la cuerda y  elimino el elemento de la variable
+        currentBallSpringJoint.enabled = false;
+        currentBallSpringJoint = null;
     }
 }
